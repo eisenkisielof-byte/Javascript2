c066492c3180c3b95c625e25822b13b3f324e8dc
eisenkisielof-byte <eisenkisielof@gmail.com>
2026-09-22 18:46:18 +0200

    Add files via upload

---
+++ kisi_zewnetrzny.js
@@ -0,0 +1,7 @@
+document.write("Mój numer w dzienniku to 6".fontcolor("#000000")+"<br>");
+document.write("Pierwszy wiersz to: 11 &nbsp;Tak &nbsp;Tak&nbsp; 5".fontcolor("#000000")+"<br>");
+document.write("Drugi wiersz to: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;10 &nbsp;&nbsp;Tak &nbsp;Nie &nbsp;5".fontcolor("#000000")+"<br>");
+document.write("ostatnia modyfikacja strony".fontcolor("olive").bold().fontsize(5) .strike()+"<br>"); 
+document.write((document.lastModified).bold().fontsize(5).fontcolor("lime"));
+
+
